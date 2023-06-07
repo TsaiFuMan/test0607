015ca9755d04c61ed01e4a5b28ad16b5d648b1be
蔡馥蔓 <s1101882@gm.pu.edu.tw>
2023-06-07 19:59:06 +0800

    add 4

---
+++ 411018824/411018824/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _411018824.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Music(string name, string number)
+        {
+            ViewBag.Name = name;
+            ViewBag.Number = number;
+            var result = "";
+            if (number == "1")
+            {
+                result = "do";
+            }else if (number == "2")
+            {
+                result = "re";
+            }
+            else if (number == "3")
+            {
+                result = "mi";
+            }
+            else if (number == "4")
+            {
+                result = "fa";
+            }
+            else if (number == "5")
+            {
+                result = "so";
+            }
+            else if (number == "6")
+            {
+                result = "la";
+            }
+            else if (number == "7")
+            {
+                result = "xi";
+            }
+            else
+            {
+                result = "錯誤";
+            }
+            ViewBag.Result = result;
+            return View();
+        }
+    }
+}
